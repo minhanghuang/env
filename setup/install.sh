265defe04c7bfe974bcc4c7d9730f5afcdf2e7c5
minhanghuang <mhesat@outlook.com>
2022-07-09 14:15:16 +0800

    feat: add setup script

---
+++ setup/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+uNames=`uname -s`
+osName=${uNames: 0: 4}
+if [ "$osName" == "Darw" ] # Darwin
+then
+	echo "Mac OS X"
+  ./Homebrew.sh
+elif [ "$osName" == "Linu" ] # Linux
+then
+	echo "GNU/Linux"
+else
+	echo "unknown os :" $osName
+  exit 8
+fi
+
+./zsh.sh
