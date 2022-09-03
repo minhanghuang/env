efd5feff3481b3f5bb7da24130a1317e5da47f3e
minhanghuang <mhesat@outlook.com>
2022-09-03 19:51:39 +0800

    fix: install zsh plugin

--- setup/zsh.plugin.sh
+++ setup/zsh.plugin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone git@gitee.com:haha-web/zsh-autosuggestions.git ~/plugins/zsh-autosuggestions
+git clone git@gitee.com:haha-web/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone git@gitee.com:haha-web/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone git@gitee.com:haha-web/autojump.git ~/plugins/autojump
+git clone git@gitee.com:haha-web/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
 git clone git@gitee.com:haha-web/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
-cd ~/plugins/autojump
+cd ~/.oh-my-zsh/custom/plugins/autojump
 python2 install.py 
 autoload -U compinit && compinit
 
