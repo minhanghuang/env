396a5686f5bbd25216b0665cd2e8c51f176b5e26
minhanghuang <mhesat@outlook.com>
2022-10-15 11:01:34 +0800

    feat: update zsh plugin gitee url

--- setup/zsh.plugin.sh
+++ setup/zsh.plugin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git clone git@gitee.com:haha-web/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone git@gitee.com:haha-web/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone git@gitee.com:haha-web/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
-git clone git@gitee.com:haha-web/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://gitee.com/haha-web/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://gitee.com/haha-web/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://gitee.com/haha-web/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
+git clone https://gitee.com/haha-web/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
 cd ~/.oh-my-zsh/custom/plugins/autojump
 python2 install.py 
 autoload -U compinit && compinit
