e23bdc9d013fcd9983ccf7602747c64783340c71
minhanghuang <mhesat@outlook.com>
2022-11-15 16:57:00 +0800

    feat: setup add google translate hosts

---
+++ setup/hosts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Google Translate
+echo "# Google Translate" >>  /etc/hosts
+echo "203.208.40.66 translate.google.com" >>  /etc/hosts
+echo "203.208.40.66 translate.googleapis.com" >> /etc/hosts
+
