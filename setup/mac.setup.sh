c1b800f5500b7396a51806c37070edaadae41929
minhanghuang <job@minhang.me>
2023-01-09 21:44:28 +0800

    perf: add mac libs

--- setup/mac.setup.sh
+++ setup/mac.setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-brew install pkg-config 
+brew install pkg-config
 brew install --HEAD universal-ctags
-brew install tig 
-brew install tree 
-brew install neovim 
-brew install node 
-brew install yarn 
-brew install clang-format 
-brew install gdb 
+brew install tig
+brew install tree
+brew install neovim
+brew install node
+brew install yarn
+brew install clang-format
+brew install gdb
 brew install libtool
+brew install autojump
+brew install lz4
 
