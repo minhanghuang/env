20f0fd94eacb612259e476b84bae743d2e57d199
minhanghuang <job@minhang.me>
2023-03-30 18:59:34 +0800

    feat: add ubuntu libs script

---
+++ setup/ubuntu.libs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install -y \
+         libgsl-dev cmake make g++ pkg-config autoconf automake iputils-ping net-tools python2.7-dev \
+         build-essential supervisor htop ntpdate chrony libtool gdb cgdb clang clang-format llvm tig tree libunwind-dev curl unzip wget \
+         zsh git vim sudo python3-pip
