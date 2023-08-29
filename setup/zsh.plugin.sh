e2e499327e11664c6ad9ecd4406f8b45b417981d
minhanghuang <job@minhang.me>
2023-08-29 13:07:45 +0800

    perf(fzf): 更新fzf安装脚本

--- setup/zsh.plugin.sh
+++ setup/zsh.plugin.sh
@@ -4,7 +4,8 @@ git clone https://gitee.com/haha-web/zsh-autosuggestions.git ~/.oh-my-zsh/custom
 git clone https://gitee.com/haha-web/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://gitee.com/haha-web/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
 git clone https://gitee.com/haha-web/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone --depth 1 https://gitee.com/haha-web/fzf.git ~/.fzf && ~/.fzf/install
+git clone --depth 1 https://gitee.com/haha-web/fzf.git ~/.fzf 
+cd ~/.fzf && ./install
 
 cd ~/.oh-my-zsh/custom/plugins/autojump
 python2 install.py
