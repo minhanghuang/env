f7942c7957a6f96c04471b7dc6189df000f51108
minhanghuang <job@minhang.me>
2023-09-08 09:17:22 +0800

    chore: git clone 设置层数

--- docker/dockerfile/18.04/base/oh-my-zsh-plugin-install.sh
+++ docker/dockerfile/18.04/base/oh-my-zsh-plugin-install.sh
@@ -3,8 +3,8 @@
 set -e
 
 # on my zsh plugins
-git clone https://gitee.com/minhanghuang/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://gitee.com/minhanghuang/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://gitee.com/minhanghuang/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth 1 https://gitee.com/minhanghuang/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sed 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' .zshrc > .zshrc.bak
 mv .zshrc.bak .zshrc
 
