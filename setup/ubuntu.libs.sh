b4bc6523ec99284a2cc7e625d4dac9d90a0c824e
minhanghuang <job@minhang.me>
2023-10-10 11:58:56 +0800

    perf(base): 更新Ubuntu基础工具

--- setup/ubuntu.libs.sh
+++ setup/ubuntu.libs.sh
@@ -11,7 +11,7 @@ sudo apt update
 sudo apt install -y \
          build-essential libgsl-dev cmake make g++ pkg-config autoconf automake iputils-ping net-tools python2.7-dev \
          supervisor htop ntpdate chrony libtool gdb cgdb clang clang-format llvm tig tree libunwind-dev curl unzip wget \
-         zsh git vim sudo python3-pip nginx nodejs npm yarn
+         zsh git vim sudo python3-pip nginx nodejs npm yarn bear axel silversearcher-ag
 
 
 sudo npm config set http://registry.npmmirror.com
