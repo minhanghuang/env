e70cef07dd1208f5745a53e4b21df727ba38b83d
minhanghuang <job@minhang.me>
2023-12-10 12:59:34 +0800

    perf(libs): 更新Ubuntu基础工具包

--- setup/ubuntu.libs.sh
+++ setup/ubuntu.libs.sh
@@ -11,8 +11,10 @@ sudo apt update
 sudo apt install -y \
          build-essential libgsl-dev cmake make g++ pkg-config autoconf automake iputils-ping net-tools python2.7-dev \
          supervisor htop ntpdate chrony libtool gdb cgdb clang clang-format llvm tig tree libunwind-dev curl unzip wget \
-         zsh git vim sudo python3-pip nginx nodejs npm yarn bear axel silversearcher-ag lsof
+         zsh git vim sudo python3-pip nginx nodejs npm yarn bear axel silversearcher-ag lsof ripgrep universal-ctags global
 
+# ripgrep: 全文搜索工具
+# universal-ctags global: 源代码导航和标签生成的工具
 
 sudo npm config set http://registry.npmmirror.com
 sudo yarn config set registry http://registry.npmmirror.com
