8662be3ecfe6a7819de5e54476cbb894cb238d08
minhanghuang <job@minhang.me>
2023-12-12 09:04:55 +0800

    feat(libs): 增加基础应用

--- setup/mac.setup.sh
+++ setup/mac.setup.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-brew install pkg-config
 brew install --HEAD universal-ctags
-brew install tig
-brew install tree
-brew install neovim
-brew install node
-brew install yarn
-brew install clang-format
-brew install gdb
-brew install libtool
-brew install autojump
-brew install lz4
-brew install nginx
+brew install pkg-config tig tree neovim nenode yarn clang-format gdb libtool autojump lz4 nginx neofetch
 
 sudo npm config set http://registry.npmmirror.com
 sudo yarn config set registry http://registry.npmmirror.com
--- setup/ubuntu.libs.sh
+++ setup/ubuntu.libs.sh
@@ -11,10 +11,11 @@ sudo apt update
 sudo apt install -y \
          build-essential libgsl-dev cmake make g++ pkg-config autoconf automake iputils-ping net-tools python2.7-dev \
          supervisor htop ntpdate chrony libtool gdb cgdb clang clang-format llvm tig tree libunwind-dev curl unzip wget \
-         zsh git vim sudo python3-pip nginx nodejs npm yarn bear axel silversearcher-ag lsof ripgrep universal-ctags global
+         zsh git vim sudo python3-pip nginx nodejs npm yarn bear axel silversearcher-ag lsof ripgrep universal-ctags global neofetch
 
 # ripgrep: 全文搜索工具
 # universal-ctags global: 源代码导航和标签生成的工具
+# neofetch: 查看系统信息
 
 sudo npm config set http://registry.npmmirror.com
 sudo yarn config set registry http://registry.npmmirror.com
