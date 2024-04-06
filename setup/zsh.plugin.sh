399d1cf51fb1182f6dee425d4cf703a3f12ec32d
minhanghuang <job@minhang.me>
2024-04-06 13:23:17 +0800

    chore: 更新zsh plugin下载链接

--- setup/zsh.plugin.sh
+++ setup/zsh.plugin.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone --depth 1 https://github.com/wting/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
-git clone --depth 1 https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+git clone --depth 1 git@github.com:zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth 1 git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth 1 git@github.com:wting/autojump.git ~/.oh-my-zsh/custom/plugins/autojump
+git clone --depth 1 git@github.com:zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone --depth 1 git@github.com:junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 cd ~/.oh-my-zsh/custom/plugins/autojump
 python2 install.py
