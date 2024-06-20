d2aaf5fbbd64b82f682f952da7551a6e2a425f98
minhanghuang <job@minhang.me>
2024-06-20 12:00:05 +0800

    feat(plugin): Added some apt tools

--- setup/ubuntu.libs.sh
+++ setup/ubuntu.libs.sh
@@ -2,29 +2,35 @@
 
 set -e
 
-apt update
+sudo apt update
 
 # install base
-apt install -y \
+sudo apt install -y \
   build-essential openssh-server net-tools libtool sudo iputils-ping libgsl-dev ntpdate \
-  python2.7-dev python3-pip python3-venv axel silversearcher-ag lsof unzip
+  python2.7-dev python3-pip python3-venv axel silversearcher-ag lsof unzip nfs-common libevent-dev
 
 # install C++ dependence
-apt install -y \
-  pkg-config autoconf automake cmake make g++ gdb cgdb clang clang-format llvm bear
+sudo apt install -y \
+  pkg-config autoconf automake cmake make g++ gdb cgdb clang clang-format llvm bear valgrind
 
 # install tools
-apt install -y \
-  htop chrony tig tree libunwind-dev curl unzip wget
+sudo apt install -y \
+  htop chrony tig tree libunwind-dev curl unzip wget baobab
 
 # install pulgin
-apt install -y \
+sudo apt install -y \
   zsh git vim nodejs npm yarn ripgrep universal-ctags global neofetch cppman
 
 # install porxy
-apt install -y supervisor nginx
+sudo apt install -y supervisor nginx
 
+# install language
+sudo apt install -y language-pack-zh-hans
+
+# nfs-common: nfs目录挂载
+# baobab: 查看磁盘占用GUI
 # ripgrep: 全文搜索工具
 # universal-ctags global: 源代码导航和标签生成的工具
 # neofetch: 查看系统信息
 # cppman: 查看C++ 98/11/14/17/20 manual pages
+# valgrind: 查看C++程序内存泄露工具
